043039e134e8faa99757c41308a55f0bca2bd96a
Ayla Ismayilzada <150772989+aylaismayilzada6@users.noreply.github.com>
2026-02-17 17:28:54 +0400

    Create a simple UNIX shell in main.c
    Implement a simple UNIX shell that reads and executes commands.

--- main.c
+++ main.c
@@ -1,13 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /**
- * main - Entry point
+ * main - Simple UNIX shell
  *
- * Return: Always 0 (Success)
+ * Description: Displays a prompt, reads commands,
+ * executes them using execve.
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	printf("Hello, Betty!\n");
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	pid_t pid;
+	int status;
+
+	while (1)
+	{
+		printf("#cisfun$ ");
+		fflush(stdout);
+
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1)
+		{
+			free(line);
+			exit(0);
+		}
+
+		if (line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
+
+		pid = fork();
+
+		if (pid == 0)
+		{
+			char *argv[] = {line, NULL};
+
+			if (execve(line, argv, NULL) == -1)
+			{
+				perror("./shell");
+				exit(1);
+			}
+		}
+		else if (pid > 0)
+		{
+			wait(&status);
+		}
+		else
+		{
+			perror("fork");
+		}
+	}
+
 	return (0);
 }
 
