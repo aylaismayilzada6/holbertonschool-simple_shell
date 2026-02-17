e838a83e5510995f52032fb0c4b41e7a1ba704c6
Ayla Ismayilzada <150772989+aylaismayilzada6@users.noreply.github.com>
2026-02-17 17:57:40 +0400

    Update main.c

--- main.c
+++ main.c
@@ -4,95 +4,175 @@
 #include <sys/wait.h>
 #include <string.h>
 
+#define MAX_ARGS 64
+
 extern char **environ;
 
 /**
- * trim_spaces - Removes leading and trailing spaces
- * @str: input string
- *
- * Return: pointer to trimmed string
+ * get_path - get PATH variable from environ
+ * Return: PATH string or NULL
  */
-char *trim_spaces(char *str)
+char *get_path(void)
 {
-	char *end;
+	int i = 0;
 
-	while (*str == ' ' || *str == '\t')
-		str++;
-
-	if (*str == '\0')
-		return (str);
-
-	end = str + strlen(str) - 1;
-
-	while (end > str && (*end == ' ' || *end == '\t'))
-		end--;
-
-	*(end + 1) = '\0';
-
-	return (str);
+	while (environ[i])
+	{
+		if (strncmp(environ[i], "PATH=", 5) == 0)
+			return (environ[i] + 5);
+		i++;
+	}
+	return (NULL);
 }
 
 /**
- * main - Simple UNIX shell
- * Return: Always 0
+ * find_path - find full path of command
+ * @cmd: command
+ * Return: full path or NULL
  */
-int main(void)
+char *find_path(char *cmd)
+{
+	char *path_env, *path_copy, *dir;
+	static char full_path[1024];
+
+	if (strchr(cmd, '/'))
+	{
+		if (access(cmd, X_OK) == 0)
+			return (cmd);
+		return (NULL);
+	}
+
+	path_env = get_path();
+	if (!path_env || *path_env == '\0')
+		return (NULL);
+
+	path_copy = strdup(path_env);
+	if (!path_copy)
+		return (NULL);
+
+	dir = strtok(path_copy, ":");
+	while (dir)
+	{
+		snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
+		if (access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		dir = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
+
+/**
+ * print_env - prints environment variables
+ */
+void print_env(void)
+{
+	int i = 0;
+
+	while (environ[i])
+	{
+		write(STDOUT_FILENO, environ[i], strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
+		i++;
+	}
+}
+
+/**
+ * main - simple shell with exit and env built-ins
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: exit status
+ */
+int main(int argc, char **argv)
 {
 	char *line = NULL;
-	char *cmd;
 	size_t len = 0;
-	ssize_t nread;
+	ssize_t read;
 	pid_t pid;
-	int status;
+	int status = 0;
+	int line_count = 0;
+	char *args[MAX_ARGS];
+	char *token;
+	char *cmd_path;
+	int i;
+
+	(void)argc;
 
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))
-		{
-			printf("#cisfun$ ");
-			fflush(stdout);
-		}
+			write(STDOUT_FILENO, "$ ", 2);
 
-		nread = getline(&line, &len, stdin);
-
-		if (nread == -1)
+		read = getline(&line, &len, stdin);
+		if (read == -1)
 		{
 			free(line);
-			exit(0);
+			exit(status);
 		}
 
-		if (line[nread - 1] == '\n')
-			line[nread - 1] = '\0';
+		line_count++;
+		line[strcspn(line, "\n")] = '\0';
 
-		cmd = trim_spaces(line);
+		i = 0;
+		token = strtok(line, " ");
+		while (token && i < MAX_ARGS - 1)
+		{
+			args[i++] = token;
+			token = strtok(NULL, " ");
+		}
+		args[i] = NULL;
 
-		if (*cmd == '\0')
+		if (args[0] == NULL)
 			continue;
 
-		pid = fork();
+		/* EXIT BUILTIN */
+		if (strcmp(args[0], "exit") == 0)
+		{
+			free(line);
+			exit(status);
+		}
 
+		/* ENV BUILTIN */
+		if (strcmp(args[0], "env") == 0)
+		{
+			print_env();
+			continue;
+		}
+
+		cmd_path = find_path(args[0]);
+
+		if (cmd_path == NULL)
+		{
+			fprintf(stderr, "%s: %d: %s: not found\n",
+				argv[0], line_count, args[0]);
+			status = 127;
+
+			if (!isatty(STDIN_FILENO))
+			{
+				free(line);
+				exit(127);
+			}
+			continue;
+		}
+
+		pid = fork();
 		if (pid == 0)
 		{
-			char *argv[2];
-
-			argv[0] = cmd;
-			argv[1] = NULL;
-
-			if (execve(cmd, argv, environ) == -1)
-			{
-				perror("./hsh");
-				exit(1);
-			}
-		}
-		else if (pid > 0)
-		{
-			wait(&status);
+			execve(cmd_path, args, environ);
+			perror(argv[0]);
+			exit(127);
 		}
 		else
 		{
-			perror("fork");
+			wait(&status);
+			status = WEXITSTATUS(status);
 		}
 	}
 
-	return (0);
+	free(line);
+	return (status);
 }
